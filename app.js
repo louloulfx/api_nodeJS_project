135954f33606208af88e4097d4d6a98b4e316b6a
louloulfx <louloulfx1@hotmail.fr>
2019-03-27 15:13:22 +0100

    cors

--- app.js
+++ app.js
@@ -3,8 +3,25 @@ const port = 5000;
 
 //Appel du fichier route avec toutes les requêtes
 const { app } = require('./routes');
-var cors = require('cors');
-app.use(cors());
+
+app.use(function (req, res, next) {
+
+    // Website you wish to allow to connect
+    res.setHeader('Access-Control-Allow-Origin', 'https://damp-hollows-18655.herokuapp.com');
+
+    // Request methods you wish to allow
+    res.setHeader('Access-Control-Allow-Methods', 'GET, POST, PUT, DELETE');
+
+    // Request headers you wish to allow
+    res.setHeader('Access-Control-Allow-Headers', 'X-Requested-With,content-type');
+
+    // Set to true if you need the website to include cookies in the requests sent
+    // to the API (e.g. in case you use sessions)
+    res.setHeader('Access-Control-Allow-Credentials', true);
+
+    // Pass to next layer of middleware
+    next();
+});
 // lancement du serveur
 app.listen(process.env.PORT || port);
 console.log('API was launched on port ' + port);
