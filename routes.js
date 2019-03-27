13afd31b667672676bf850058cf52c74e6653f36
louloulfx <louloulfx1@hotmail.fr>
2019-03-27 10:49:19 +0100

    start

--- routes.js
+++ routes.js
@@ -1,29 +1,43 @@
 //Appel du module express
-const express = require('express');
+const express = require("express");
 
 //Appel du module bodyParser
-const bodyParser = require('body-parser');
+const bodyParser = require("body-parser");
 
 //Appel des constantes afin de créer les requêtes get, put, getbyid, delete et put
-const { providerPost, providerGet, providerDelete, providerGetbyId, providerPut } = require('./routes/providerRoutes')
-const { userPost, userGet, userDelete, userGetbyId, userPut } = require('./routes/userRoutes')
+const {
+  providerPost,
+  providerGet,
+  providerDelete,
+  providerGetbyId,
+  providerPut
+} = require("./routes/providerRoutes");
+const {
+  userPost,
+  userGet,
+  userDelete,
+  userGetbyId,
+  userPut
+} = require("./routes/userRoutes");
 
 const app = express();
 app.use(bodyParser.json());
 
-app.get('/', res.redirect('/user'));
+app.get("/", function(req, res) {
+  res.redirect("/user");
+});
 //Création des requêtes pour tout les modules avec express
-app.post('/provider', providerPost);
-app.get('/provider', providerGet);
-app.delete('/provider/:id', providerDelete);
-app.get('/provider/:id', providerGetbyId);
-app.put('/provider/:id', providerPut);
+app.post("/provider", providerPost);
+app.get("/provider", providerGet);
+app.delete("/provider/:id", providerDelete);
+app.get("/provider/:id", providerGetbyId);
+app.put("/provider/:id", providerPut);
 
-app.post('/user', userPost);
-app.get('/user', userGet);
-app.delete('/user/:id', userDelete);
-app.get('/user/:id', userGetbyId);
-app.put('/user/:id', userPut);
+app.post("/user", userPost);
+app.get("/user", userGet);
+app.delete("/user/:id", userDelete);
+app.get("/user/:id", userGetbyId);
+app.put("/user/:id", userPut);
 
 //Export de express et de toutes les routes
 module.exports = { app };
