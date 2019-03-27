922abfbdbae8b179901a00db9870cf060ad37737
louloulfx <louloulfx1@hotmail.fr>
2019-03-27 16:10:27 +0100

    cors 2

--- app.js
+++ app.js
@@ -1,10 +1,12 @@
 //Constante du port
 const port = 5000;
-var cors = require('cors');
 //Appel du fichier route avec toutes les requêtes
 const { app } = require('./routes');
-
-app.use(cors());
+app.use(function(request, response, next) {
+    response.header("Access-Control-Allow-Origin", "*");
+    response.header("Access-Control-Allow-Headers", "Origin, X-Requested-With, Content-Type, Accept");
+    next();
+});
 // lancement du serveur
 app.listen(process.env.PORT || port);
 console.log('API was launched on port ' + port);
