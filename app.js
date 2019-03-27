e7a9697e688b1fabc42bade81b4d7162dddd0f72
louloulfx <louloulfx1@hotmail.fr>
2019-03-27 11:04:26 +0100

    start

--- app.js
+++ app.js
@@ -1,9 +1,9 @@
 //Constante du port
-const port = 3000;
+const port = 5000;
 
 //Appel du fichier route avec toutes les requêtes
 const { app } = require('./routes');
 
 // lancement du serveur
-app.listen(port);
+app.listen(process.env.PORT || port);
 console.log('API was launched on port ' + port);
